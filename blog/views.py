2369635bf2f1a393fb63475439ae9ec6b5c5ed6a
hassan-fiaz <mhas4908@gmail.com>
2020-07-18 09:47:21 +0500

    add static file and bootstrap design

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+posts = [
+	{
+		'author': 'Hassan',
+		'title': 'Blog Post 1',
+		'content': 'First post content',
+		'date_posted': 'July 17, 2020'
+	},
+	{
+		'author': 'Hassan',
+		'title': 'Blog Post 2',
+		'content': 'First post content',
+		'date_posted': 'July 16, 2020'
+	},
+]
 
 
 def home(request):
 	template = 'blog/home.html'
-	context = {'title': 'Home'}
+	context = {'title': 'Home', 'posts': posts}
 	return render(request, template, context)
 
 def about(request):
